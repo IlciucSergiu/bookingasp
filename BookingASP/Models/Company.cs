5996d3a7d2a50ec6dca245847df91638a7968728
Ilciuc Sergiu <eu_sergiuu14@yahoo.com>
2017-07-12 16:36:58 +0300

    initail commit

---
+++ BookingASP/Models/Company.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.IO;
+using System.Drawing;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace BookingASP.Models
+{
+    public class Company
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(18, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^ ((?=.*[a - z])(?=.*[A - Z])(?=.*\d)).+$")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Logo { get; set; }
+
+       
+
+        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+        }
+
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+
+    public class CompanyDBContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+    }
+
+
+}
